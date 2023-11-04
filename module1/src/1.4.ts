729fba00c6c938ee145b9fa94d9a02d5e0efbb09
BakiAbdullah <shatil605@gmail.com>
2023-11-04 13:00:33 +0600

    tuple ts data types practicing

---
+++ module1/src/1.4.ts
@@ -0,0 +1,37 @@
+// Basic Data types (Module 1.4)
+
+//String
+let firstName: string = 'Baki'
+
+//Number
+let roll:number = 123
+
+//boolean
+let isAdmin:boolean = true
+
+//undefined
+let x: undefined = undefined
+
+//null
+let y:null = null
+
+let d:number;
+
+d= 123;
+// d='ddd'
+// d= true
+
+let friends: string[] = ['abc','def', 'ref'];
+
+
+let roleList: number[] = [1,2]
+
+//* Tuple is a type of array
+// tuple --> array --> order --> type of values
+
+let coordinates: [number, number] = [1,5]
+let ageName: [number, string, boolean] = [50, 'Baki', true]; // tuple data type in TypeScript
+ageName[0] = 34
+
+
+
