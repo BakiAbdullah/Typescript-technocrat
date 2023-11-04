9e13f693416308f2e49a48d52bdc924fcbc67ed6
BakiAbdullah <shatil605@gmail.com>
2023-11-04 13:38:39 +0600

    type literal, optional type

---
+++ module1/src/1.5.ts
@@ -0,0 +1,16 @@
+// Reference Type => Object
+
+const user : {
+  company: 'Programming Hero'; //** Literal types or predefined values */
+  firstName : string;
+  middleName?: string; //** Optional Type (Thakteo pare nao thakte pare) */
+  lastName : string;
+  isMarried: boolean;
+} = {
+  company: 'Programming Hero',
+  firstName: 'Md',
+  // middleName: 'Abdullahil',
+  lastName : 'Baki',
+  isMarried: true,
+}
+
