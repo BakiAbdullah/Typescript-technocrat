5d587cb522e32e4cf1a0f7837345100a7f52b442
BakiAbdullah <shatil605@gmail.com>
2023-11-04 23:59:42 +0600

    array and object destructuring

--- module1/src/1.8.ts
+++ module1/src/1.8.ts
@@ -1,1 +1,23 @@
-//
+//**Rest operator in Typescript */
+
+{
+  const user = {
+  id: 345,
+  name: {
+      firstName: "Md",
+      middleName: "Sakib",
+      lastName: "khan",
+   },
+   contactNo: "01952334338",
+   address: 'Nepal'
+}; 
+
+const { contactNo, name: {middleName} } = user
+
+// Array destructuring
+
+const myFriends = ['mike', 'joe', 'ross', 'rachel', 'jon']
+
+const [a,b,bestFriend, ...rest] = myFriends;
+
+}
