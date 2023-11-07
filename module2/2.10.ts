0b1232c0f3b89dbb0bc488faaab0d2586430d0a8
BakiAbdullah <shatil605@gmail.com>
2023-11-07 15:36:31 +0600

    mapped types in TS

--- module2/2.10.ts
+++ module2/2.10.ts
@@ -12,9 +12,24 @@
 type AreaNumber = {
   height: number;
   width: number;
-}
+};
+
+type Height = AreaNumber["width"]; // look up type
+
 // type AreaString = {
 //   height: string;
 //   width: string;
 // }
 
+//** Mapped types */ 
+type AreaString<T> = {
+  [key in keyof T]: T[key]; //* type Height = AreaNumber["width"]; // look up type
+};
+
+const area1: AreaString<{
+  height: string;
+  width: number;
+}> = {
+  height: '100',
+  width: 200
+};
