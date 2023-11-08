11015a20da91c60158a54edf94df243ec96c4455
BakiAbdullah <shatil605@gmail.com>
2023-11-09 01:09:38 +0600

    Statics in OOP

--- module3/3.6.ts
+++ module3/3.6.ts
@@ -1,33 +1,42 @@
 {
   {
-  //** getter and setter in OOP */
-  class BankAccount {
-    public readonly id: number;
-    public name: string;
-    protected _balance: number;
+    //** getter and setter in OOP */
+    class BankAccount {
+      public readonly id: number;
+      public name: string;
+      protected _balance: number;
 
-    constructor(id: number, name: string, _balance: number) {
-      this.id = id;
-      this.name = name;
-      this._balance = _balance;
+      constructor(id: number, name: string, _balance: number) {
+        this.id = id;
+        this.name = name;
+        this._balance = _balance;
+      }
+
+      //*** functions in OOP using setter ***/
+      set addDeposit(amount: number) {
+        this._balance = this._balance + amount;
+      }
+
+      // public addDeposit(amount: number) {
+      //   this._balance = this._balance + amount;
+      // }
+
+      //*** functions in OOP using getter ***/
+      public get balance() {
+        return this._balance;
+      }
+
+      // public getBalance() {
+      //   return this._balance;
+      // }
     }
 
-   public addDeposit(amount: number) {
-      this._balance = this._balance + amount;
-    }
+    const myBankAccount = new BankAccount(1234, "Mr. zzz", 100);
+    // myBankAccount.addDeposit(100); //! Calling normal function
+    myBankAccount.addDeposit = 50; //! Calling setter function
+    // const myBalance = myBankAccount.getBalance();
+    const myBalance = myBankAccount.balance; //! Calling getter function
 
-   public getBalance() {
-      return this._balance;
-    }
+    console.log(myBalance);
   }
-
- 
-
-  const myBankAccount = new BankAccount(1234, "Mr. zzz", 100);
-  myBankAccount.addDeposit(100);
-  const myBalance = myBankAccount.getBalance();
-  console.log(myBalance);
-  //
-}
-
 }
--- module3/3.7.ts
+++ module3/3.7.ts
@@ -1,4 +1,22 @@
 {
-  //** */
+  //** Statics in OOP */
+
+  class Counter {
+    static count: number = 0; // by mentioning static it will be refered to single memory
+
+    static increment() {
+      return (Counter.count = Counter.count + 1);
+    }
+    static decrement() {
+      return (Counter.count = Counter.count - 1);
+    }
+  }
+
+  // const instance1 = new Counter();
+  console.log(Counter.increment()); // 1 => different memory usage
+
+  // const instance2 = new Counter();
+  console.log(Counter.increment()); // 1 => different memory usage
+
   //
 }
