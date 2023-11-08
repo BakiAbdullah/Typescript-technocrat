c1319bec5a9204aaff3ea2a733eb13d1e591c79f
BakiAbdullah <shatil605@gmail.com>
2023-11-09 00:36:02 +0600

    getter and setter in OOP

--- module3/3.5.ts
+++ module3/3.5.ts
@@ -1,18 +1,35 @@
 {
   //** Access modifiers */
   class BankAccount {
-   readonly id: number;
-    name: string;
-    balance: number;
+    public readonly id: number;
+    public name: string;
+    protected _balance: number;
 
-    constructor(id: number, name: string, balance: number) {
+    constructor(id: number, name: string, _balance: number) {
       this.id = id;
       this.name = name;
-      this.balance = balance;
+      this._balance = _balance;
+    }
+
+   public addDeposit(amount: number) {
+      this._balance = this._balance + amount;
+    }
+
+   public getBalance() {
+      return this._balance;
     }
   }
 
+  // extending child account from myBankAccount
+  // class StudentAccount extends BankAccount {
+  //   test(){
+  //     this._balance
+  //   }
+  // }
+
   const myBankAccount = new BankAccount(1234, "Mr. zzz", 100);
-  myBankAccount.id = 
+  myBankAccount.addDeposit(100);
+  const myBalance = myBankAccount.getBalance();
+  console.log(myBalance);
   //
 }
